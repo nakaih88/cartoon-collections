018fbac57b0b91b407fcb6b8282ee6fcffcdd3e7
Nakai <nakai.hamlin18@gmail.com>
2021-02-25 19:36:27 -0500

    commit tree 70400e0b89f0260cf012fc3f46297cfdaf4aeedc

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do | dwarf, i |
+    puts "#{i}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map do |names|
+    names.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  true_or_false = false
+  calls_long.each do |calls|
+    if calls.length > 4
+      true_or_false = true
+    end
+  end
+  true_or_false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
